92ea07e6ed026573ffa64e2476dbc8070cad4a82
Justus <justusjansen94@gmail.com>
2018-03-07 11:21:27 +0100

    background rebeccapurple

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<div style='background: blue;'>Hello, world<br>This is a test file.</div>";
+echo "<div style='background: rebeccapurple;'>Hello, world<br>This is a test file.</div>";
 
 ?>
 <a href="index2.php">index 2</a>
