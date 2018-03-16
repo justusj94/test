71d006bfb65c0772494e85f3ea6d7534810d6f8c
Justus <justusjansen94@gmail.com>
2018-03-16 09:43:41 +0100

    no message

--- index2.php
+++ index2.php
@@ -10,10 +10,7 @@ echo "more info yet";
 ?>
 <div style="background-color: skyblue;"><a href="index.php">index</a></div>
 <br>
-<<<<<<< HEAD
-<div style="background-color: deeppink">
-=======
+
 <div style="background-color: darkgoldenrod">
->>>>>>> test
     Nice random info!
 </div>
