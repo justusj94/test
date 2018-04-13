29c7fa42936c27a6a3a72a63ae7ae178818cdc1a
Justus <justusjansen94@gmail.com>
2018-04-13 09:53:29 +0200

    no message

--- karma.conf.js
+++ karma.conf.js
@@ -11,10 +11,16 @@ module.exports = function(config) {
             'karma-phantomjs-launcher'
         ],
         preprocessors: {},
-        reporters: ['progress', 'junit'],
+        reporters: ['progress', 'dots', 'junit'],
         junitReporter: {
             outputDir: 'results',
-            outputFile: 'test-results.xml'
+            outputFile: 'test-results.xml',
+            suite: '', // suite will become the package name attribute in xml testsuite element
+            useBrowserName: true, // add browser name to report and classes names
+            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+            classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+            properties: {},
+            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
         },
         port: 9876,
         colors: true,
