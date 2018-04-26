173686c8569f90e9e57be53315fe84a39cf84052
Justus <justusjansen94@gmail.com>
2018-04-26 10:15:31 +0200

    google_search change

--- selenium-tests/google_search.js
+++ selenium-tests/google_search.js
@@ -1,18 +1,11 @@
 const {Builder, By, Key} = require('selenium-webdriver');
 const browsers = ['firefox', 'chrome'];
 
-
-
-
-for(let x = 0; x < x.length ; x++) {
-    testBrowsers(browsers[x])
-}
-
-function testBrowsers(browser){
-    (async function example() {
+(async function example() {
+    for(let x = 0; x < x.length ; x++) {
         //let driver = await new Builder().forBrowser('chrome').build();
-        console.log('Start Google search test on ' + browser);
-        let driver = await new Builder().forBrowser(browser).usingServer('http://172.17.0.3:4444/wd/hub').build();
+        console.log('Start Google search test on ' + browsers[x]);
+        let driver = await new Builder().forBrowser(browsers[x]).usingServer('http://172.17.0.3:4444/wd/hub').build();
         try {
             await driver.get('http://www.google.com/ncr');
             console.log('Search for webdriver');
@@ -21,20 +14,20 @@ function testBrowsers(browser){
             await driver.wait(check_title(driver), 1000);
         } finally {
             await driver.quit();
-            console.log('End Google search test on ' + browser);
+            console.log('End Google search test on ' + browsers[x]);
         }
-    })();
-
-    function check_title(driver) {
-        let promise = driver.getTitle().then((title) => {
-            if (title === 'webdriver - Google Search' ) {
-                console.log('Success -- ' + title);
-                return true;
-            }
-            else {
-                console.log('Fail -- ' + title);
-            }
-        });
-        return promise;
     }
+})();
+
+function check_title(driver) {
+    let promise = driver.getTitle().then((title) => {
+        if (title === 'webdriver - Google Search' ) {
+            console.log('Success -- ' + title);
+            return true;
+        }
+        else {
+            console.log('Fail -- ' + title);
+        }
+    });
+    return promise;
 }
