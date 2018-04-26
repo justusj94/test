8e32d48cbbdbd72dc033525a50e77b9e177fb7a3
Justus <justusjansen94@gmail.com>
2018-04-26 10:27:37 +0200

    google_search change

--- selenium-tests/google_search.js
+++ selenium-tests/google_search.js
@@ -2,21 +2,21 @@ const {Builder, By, Key} = require('selenium-webdriver');
 const browsers = ['firefox', 'chrome'];
 
 (async function example() {
-    for(let x = 0; x < x.length ; x++) {
-        //let driver = await new Builder().forBrowser('chrome').build();
-        console.log('Start Google search test on ' + browsers[x]);
-        let driver = await new Builder().forBrowser(browsers[x]).usingServer('http://172.17.0.3:4444/wd/hub').build();
-        try {
-            await driver.get('http://www.google.com/ncr');
-            console.log('Search for webdriver');
-            await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-            await driver.findElement(By.name('btnK')).click();
-            await driver.wait(check_title(driver), 1000);
-        } finally {
-            await driver.quit();
-            console.log('End Google search test on ' + browsers[x]);
-        }
+    //for(let x = 0; x < x.length ; x++) {
+    //let driver = await new Builder().forBrowser('chrome').build();
+    //console.log('Start Google search test on ' + browsers[x]);
+    let driver = await new Builder().forBrowser('chrome').usingServer('http://172.17.0.3:4444/wd/hub').build(); //http://172.17.0.3:4444/wd/hub
+    try {
+        await driver.get('http://www.google.com/ncr');
+        console.log('Search for webdriver');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.findElement(By.name('btnK')).click();
+        await driver.wait(check_title(driver), 1000);
+    } finally {
+        await driver.quit();
+        //     console.log('End Google search test on ' + browsers[x]);
     }
+    // }
 })();
 
 function check_title(driver) {
