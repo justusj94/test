db63d78dee876eb98a047687292a6bd12157ce44
Justus <justusjansen94@gmail.com>
2018-05-07 11:39:49 +0200

    asdasd

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,88 @@
+// Karma configuration
+// Generated on Thu May 03 2018 09:16:44 GMT+0200 (West-Europa (zomertijd))
+
 module.exports = function(config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine'],
-        files: [ 'tests/**/*.test.js'],
-        exclude: [
-        ],
-        plugins: [
-            'karma-jasmine',
-            'karma-junit-reporter',
-            'karma-phantomjs-launcher'
-        ],
-        preprocessors: {},
-        reporters: ['progress', 'dots', 'junit'],
-        junitReporter: {
-            outputDir: 'results',
-            outputFile: 'test-results.xml',
-            suite: '', // suite will become the package name attribute in xml testsuite element
-            useBrowserName: true, // add browser name to report and classes names
-            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
-            classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
-            properties: {},
-            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
-        },
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['PhantomJS'],
-        singleRun: true
-    });
-};
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'tests/**/*.test.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // list of plugins
+    plugins: [
+      'karma-jasmine',
+      'karma-junit-reporter',
+      'karma-phantomjs-launcher'
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+    junitReporter: {
+      outputDir: 'results',
+      outputFile: 'test-results.xml',
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {},
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: [
+      'progress',
+      'dots',
+      'junit'
+    ],
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
